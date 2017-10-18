6928bc74092b804a5e792c2095875c1d0cf214d3
Laurent <Laurentmichel@me.com>
2017-10-18 10:51:19 +0200

    Optimized code as Maarten suggested

--- src/button/mqtt_sender.py
+++ src/button/mqtt_sender.py
@@ -1,6 +1,8 @@
 import src.irulez.constants as constants
 import src.irulez.log as log
 import json
+from typing import List, Dict, Optional
+import src.irulez.domain as domain
 
 logger = log.get_logger('button_mqtt_sender')
 
@@ -10,28 +12,24 @@ class MqttSender:
         self.client = client
         self.arduinos = arduinos
 
-    def publish_relative_action(self, json_list: {}):
+    def publish_relative_action(self, json_list: Dict[str, domain.IndividualAction]):
         for name in json_list:
             for i in range(len(json_list[name])):
-                if json_list[name][i].delay == 0:
-                    payload = json.dumps(
-                        {
-                            "name": name,
-                            "topic": constants.arduinoTopic + '/' + name + '/' + constants.actionTopic,
-                            "on": json_list[name][i].pin_numbers_on, "off": json_list[name][i].pin_numbers_off,
-                            "delay": json_list[name][i].delay})
-                    topic = constants.arduinoTopic + '/' + constants.actionTopic + '/' + constants.relative
-                else:
-                    payload = json.dumps(
-                        {
-                            "name": name,
-                            "topic": constants.arduinoTopic + '/' + name + '/' + constants.actionTopic + '/' + constants.relative,
-                            "on": json_list[name][i].pin_numbers_on, "off": json_list[name][i].pin_numbers_off,
-                            "delay": json_list[name][i].delay})
-                    topic = constants.arduinoTopic + '/' + constants.actionTopic + '/' + constants.relative + '/' + constants.timer
+                topic_name = constants.arduinoTopic + '/' + name + '/' + constants.actionTopic
+                publish_topic = constants.arduinoTopic + '/' + constants.actionTopic + '/' + constants.relative
+                if json_list[name][i].delay != 0:
+                    topic_name = topic_name + '/' + constants.relative
+                    publish_topic = publish_topic + '/' + constants.timer
 
-                logger.debug(f"Publishing: {topic}{payload}")
-                self.client.publish(topic, payload, 0, False)
+                payload = json.dumps(
+                    {
+                        "name": name,
+                        "topic": topic_name,
+                        "on": json_list[name][i].pin_numbers_on, "off": json_list[name][i].pin_numbers_off,
+                        "delay": json_list[name][i].delay})
+
+                logger.debug(f"Publishing: {publish_topic}{payload}")
+                self.client.publish(publish_topic, payload, 0, False)
 
 
 
--- src/relative_convertor/mqtt_sender.py
+++ src/relative_convertor/mqtt_sender.py
@@ -3,6 +3,7 @@ import src.irulez.constants as constants
 import src.irulez.log as log
 from typing import List, Dict, Optional
 import json
+import src.irulez.domain as domain
 
 logger = log.get_logger('relative_convertor_mqtt_sender')
 
@@ -12,23 +13,18 @@ class MqttSender:
         self.client = client
         self.arduinos = arduinos
 
-    def publish_absolute_action(self, json_object: {}, absolute: []):
+    def publish_absolute_action(self, json_object: Dict[str, object], absolute: List[bool]):
         payload = util.convert_array_to_hex(absolute)
         topic = json_object['topic']
         logger.debug(f"Publishing: {topic}/{payload}")
         self.client.publish(topic, payload, 0, False)
 
-    def send_absolute_update(self, arduino, json_object):
+    def send_absolute_update(self, arduino: domain.Arduino, json_object: Dict[str, object]):
 
         # Accepts relative pins as input, converts them to absolute updates
         # TODO: improve 'send_update' mechanism to only send updates to arduinos with updates.
         #  Current implementation sends updates to all arduinos or none.
         send_update = False
-
-        if arduino is None:
-            # Unknown arduino
-            logger.info(f"Could not find arduino with name '{name}'.")
-            # Continue means hop to the next cycle of the for loop
 
         absolute = [False] * arduino.number_of_output_pins
         for pin in arduino.output_pins.values():
