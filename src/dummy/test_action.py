bdff187b4a8ddc2762154a013a8592031ed9a249
Laurent <Laurentmichel@me.com>
2017-10-25 15:18:28 +0200

    Expand test_action to work with multiple clicks and long down

--- src/dummy/test_action.py
+++ src/dummy/test_action.py
@@ -1,9 +1,11 @@
 import lib.paho.mqtt.publish as publish
-import src.irulez.db
+import time
 import src.irulez.constants as constants
 import src.irulez.util as util
 import src.irulez.configuration as configuration
-import lib.mysql.connector as mariadb
+import src.irulez.log as logger
+
+log = logger.get_logger("test_action")
 
 # Connect
 config = configuration.Configuration()
@@ -12,32 +14,80 @@ databaseConfig = config.get_database_config()
 
 
 
-arduino = input("Arduino name? <DEMO|virtual_IO_Board> ")
-number_of_pin = input("Numver of pins? <16|20>")
-pin = input("Pin ")
-button_action = input("B: Button , A: action ")
+arduino = input("Arduino name? <[DEMO]|virtual_IO_Board> ")
+number_of_pin = input("Number of pins? <[16]|20> ")
+pin = input("Pin [10]")
+button_type = input("C: [CLICK], L: LOW, H: HIGH M : Multi-click ")
+if button_type == '':
+    button_type = 'C'
+
+if arduino == '':
+    arduino = "DEMO"
+if number_of_pin == '':
+    number_of_pin = 16
+if pin == '':
+    pin = 10
+
 
 pin_states = [0] * int(number_of_pin)
+pin_states_Release = [0] * int(number_of_pin)
 
-if (pin != ""):
-    list = pin.split('|');
-    for relay in list:
-        pin_states[int(relay)] = 1
+
+pin_states[int(pin)] = 1
 
 payload = util.convert_array_to_hex(pin_states)
+payload_release = util.convert_array_to_hex(pin_states_Release)
 
-if (button_action == "A"):
-    print(constants.arduinoTopic + "/" + arduino + "/" + constants.actionTopic + "/" + payload)
 
-    publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.actionTopic, payload,
-                   auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
-                   hostname=mqttConfig['ip'],
-                   port=mqttConfig['port'], retain=False)
 
-elif (button_action == "B"):
-    print(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload)
-
+if button_type == 'C':
+    log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload)
     publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload,
                    auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
                    hostname=mqttConfig['ip'],
                    port=int(mqttConfig['port']), retain=False)
+    time.sleep(1)
+    log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload_release)
+    publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload_release,
+                   auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
+                   hostname=mqttConfig['ip'],
+                   port=int(mqttConfig['port']), retain=False)
+
+if button_type == 'L':
+    lowtimer = input("Low for how many sec, 0 for forever? [0] ")
+    if lowtimer == '':
+        lowtimer = 0
+    log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload)
+    publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload,
+                   auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
+                   hostname=mqttConfig['ip'],
+                   port=int(mqttConfig['port']), retain=False)
+    time.sleep(int(lowtimer))
+    log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload_release)
+    publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload_release,
+                   auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
+                   hostname=mqttConfig['ip'],
+                   port=int(mqttConfig['port']), retain=False)
+
+if button_type == 'H':
+    log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload_release)
+    publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload_release,
+                   auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
+                   hostname=mqttConfig['ip'],
+                   port=int(mqttConfig['port']), retain=False)
+
+if button_type == 'M':
+    clicks = input("How many clicks do you want to trigger? [3] ")
+    if clicks == '':
+        clicks = 3
+    for x in range(0, clicks):
+        log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload)
+        publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload,
+                       auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
+                       hostname=mqttConfig['ip'],
+                       port=int(mqttConfig['port']), retain=False)
+        log.info(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic + "/" + payload_release)
+        publish.single(constants.arduinoTopic + "/" + arduino + "/" + constants.buttonTopic, payload_release,
+                       auth={'username': mqttConfig['username'], 'password': mqttConfig['password']},
+                       hostname=mqttConfig['ip'],
+                       port=int(mqttConfig['port']), retain=False)
