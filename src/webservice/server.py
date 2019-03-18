fd0656c11543004d72c42c76111093795c70c0f7
Laurent <laurentmichel@me.com>
2019-03-18 10:24:38 +0100

    Server

---
+++ src/webservice/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Thisissecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:irulez4database@10.0.50.50/iRulez'
+
+db = SQLAlchemy(app)
+
+
+class User2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
